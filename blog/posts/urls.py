ea3bc9032f31725c869b3f9bd954eb2bf1bbd447
unknown <aylentorres27@gmail.com>
2022-03-06 23:35:54 -0300

    Commiting new changes

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('post/<str:pk>', views.post, name='post')
 ]
